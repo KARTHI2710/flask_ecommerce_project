b4ecf7e942913dc24d48e1d473c783f6d4b8d1c2
KARTHI2710 <bskarthikeyan2710@gmail.com>
2024-05-15 19:21:57 +0530

    first commit

---
+++ shop/admin/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template,redirect,url_for,request,session,flash
+from shop import app,db
+from .forms import RegistrationForm
+
+@app.route('/')
+def home():
+    return "Home page of my shop"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # user = User(form.username.data, form.email.data,
+        #             form.password.data)
+        # db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('admin/register.html', form=form , title="Registration form")
