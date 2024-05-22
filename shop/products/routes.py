d093a96865e4294eb2045dc9b32329eaebaea454
KARTHI2710 <92161117+KARTHI2710@users.noreply.github.com>
2024-05-22 08:00:49 +0530

    Addproduct Model added

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template,redirect,url_for,flash,request
 from shop import app,db
-from .models import Brand,Category
+from .models import Brand,Category,Addproduct
 from .forms import AddProductForm 
 
 @app.route('/addbrand',methods=['GET','POST'])
@@ -34,4 +34,31 @@ def addproduct():
     form=AddProductForm(request.form)
     brands=Brand.query.all()
     categories=Category.query.all()
+
+    if request.method == 'POST':
+        name=form.name.data
+        price=form.price.data
+        discount=form.discount.data
+        stock=form.stock.data
+        colors=form.colors.data
+        desc = form.desc.data
+        brand = request.form.get('brand')
+        category = request.form.get('category')
+        # image_1 =
+        # image_2 =
+        # image_3 = 
+
+        addpro=Addproduct(
+            name=name,
+            price=price,
+            discount=discount,
+            stock=stock,
+            colors=colors,
+            desc=desc,
+            brand_id=brand,
+            category_id=category
+        )
+        db.session.add(addpro)
+        flash(f'The product {name} has been added to your database','success')
+        return redirect(url_for('home'))
     return render_template('products/addproduct.html',form=form,title="Add Product",brands=brands,categories=categories)
